2f849a9500ed99af8105a30a0361e44b5a2f0e24
vitor goncalves <vitorgoncalves.991@gmail.com>
2024-01-22 11:42:20 -0300

    fix: use float ui more correct

--- scripts/app.js
+++ scripts/app.js
@@ -1,44 +1,17 @@
-import {computePosition , offset, arrow , autoPlacement} from 'https://cdn.jsdelivr.net/npm/@floating-ui/dom@1.5.4/+esm';
+import {computePosition , offset, arrow } from 'https://cdn.jsdelivr.net/npm/@floating-ui/dom@1.5.4/+esm';
+
+const MOBILE_WIDTH_FOOTER = 500;
 window.onload = (e) => {
 
-    
-    
     const btnShowActiveFooter = document.getElementById('shared');
-    const tooltip = document.getElementById('tooltip');
-    const arrowElement = document.getElementById('arrow');
+    
 
-    computePosition(btnShowActiveFooter, tooltip, {
-        placement:'top',
-         middleware:[arrow({element:arrowElement})]
-    }).then(({x, y, placement, middlewareData}) => {
-            
-            Object.assign(tooltip.style, {
-            left: `${x -90}px`,
-            top: `${y -90}px`,
-            });
-            const {x: arrowX, y: arrowY} = middlewareData.arrow;
- 
-            const staticSide = {
-              top: 'bottom',
-              right: 'left',
-              bottom: 'top',
-              left: 'right',
-            }[placement.split('-')[0]];
-           
-            Object.assign(arrowElement.style, {
-              left: arrowX != null ? `${arrowX}px` : '',
-              top: arrowY != null ? `${arrowY}px` : '',
-              right: '10px',
-              bottom: '',
-              [staticSide]: '-4px',
-            });
-     });
     
     btnShowActiveFooter.onclick = (e) => {
         
         const largura = window.screen.width;
-        console.log(largura)
-        if(largura <= 500 ){
+        
+        if(largura <= MOBILE_WIDTH_FOOTER ){
         const actualFooter = document.getElementsByClassName("footer-mobile")[0];
         
         actualFooter.style.display = "none";
@@ -52,6 +25,36 @@ window.onload = (e) => {
             if(tooltip.style.display == 'flex'){
                 tooltip.style.display = 'none'
             }else{
+                const tooltip = document.getElementById('tooltip');
+                const arrowElement = document.getElementById('arrow');
+
+                computePosition(btnShowActiveFooter, tooltip, {
+                    placement:'top',
+                     middleware:[arrow({element:arrowElement}),offset(20)]
+                }).then(({x, y, placement, middlewareData}) => {
+                        
+                        Object.assign(tooltip.style, {
+                        left: `${x}px`,
+                        top: `${y}px`,
+                        });
+                        const {x: arrowX, y: arrowY} = middlewareData.arrow;
+             
+                        const staticSide = {
+                          top: 'bottom',
+                          right: 'left',
+                          bottom: 'top',
+                          left: 'right',
+                        }[placement.split('-')[0]];
+                       
+                        Object.assign(arrowElement.style, {
+                          left: arrowX != null ? `${arrowX}px` : '',
+                          top: arrowY != null ? `${arrowY}px` : '',
+                          right: '',
+                          bottom: '',
+                          [staticSide]: '-4px',
+                        });
+                 });
+
                 tooltip.style.display = 'flex';
             }
             
@@ -70,9 +73,6 @@ window.onload = (e) => {
         
         actualFooter.style.display = "flex";
         actualFooter.style.transitions = "1s";
-
-        
-
     }
 
 }
